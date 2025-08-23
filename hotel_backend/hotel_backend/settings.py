ef146fcd30e8c8635f5a9ac0bfa4b6e4bd001134
darkside-D77 <darksidejd7@gmail.com>
2025-08-22 22:00:01 -0500

    Quitar SessionAuthentication de DRF para eliminar CSRF requirement

--- hotel_backend/hotel_backend/settings.py
+++ hotel_backend/hotel_backend/settings.py
@@ -165,8 +165,8 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 # Django REST Framework Configuration
 REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': [
-        'rest_framework.authentication.SessionAuthentication',
         'rest_framework.authentication.TokenAuthentication',
+        # 'rest_framework.authentication.SessionAuthentication',  # Comentado para evitar CSRF
     ],
     'DEFAULT_PERMISSION_CLASSES': [
         'rest_framework.permissions.IsAuthenticated',
